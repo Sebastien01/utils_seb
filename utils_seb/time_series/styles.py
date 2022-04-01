7e2f9ae8f3649bf3d393b7c948924851de6dfbe6
Sebastien Vallin <seb.vallin@orange.fr>
2022-04-01 14:59:41 +0200

    added styles

---
+++ utils_seb/time_series/styles.py
@@ -0,0 +1,5 @@
+plot_params = {'color': '0.75',
+                'style': '.-',
+                'markeredgecolor': '0.25',
+                'markerfacecolor': '0.25',
+                'legend': False}
