d24c86449227a4e6c3f128b5a7025a78c516b144
Sébastien VALLIN <sebastien.vallin@FRARTFVFKV25Z1WG7-M.local>
2023-07-28 11:47:05 +0200

    add: theme template and aliases for ohmyzsh

---
+++ ohmyzsh_configs/aliases_and_shortcuts.zsh
@@ -0,0 +1,10 @@
+# /Users/sebastien.vallin/.oh-my-zsh/custom/aliases_and_shortcuts.zsh          (Can be accessed with cd $ZSH_CUSTOM)
+
+# Custom aliases
+alias jn="jupyter notebook"
+alias -g G="| grep "
+alias gcm="git commit -m"
+
+# Set shortcuts
+# start_directory="/users/sebastien.vallin/code"
+# cd $start_directory
